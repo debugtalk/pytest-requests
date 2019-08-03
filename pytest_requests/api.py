3f8f463571a6d3843ba9b3f58352c79ae9ce7a2a
debugtalk <mail@debugtalk.com>
2019-08-03 22:56:10 +0800

    change: make BaseApi class varialbes consistent with requests.request kwargs

--- pytest_requests/api.py
+++ pytest_requests/api.py
@@ -5,11 +5,20 @@ class BaseApi(object):
 
     method = "GET"
     url = ""
-    params = {}
-    headers = {}
-    cookies = {}
-    data = {}
-    json = {}
+    params = None
+    data = None
+    headers = None
+    cookies = None
+    files = None
+    auth = None
+    timeout = None
+    allow_redirects = True
+    proxies = None
+    hooks = None
+    stream = None
+    verify = None
+    cert = None
+    json = None
 
     def __init__(self):
         self.response = None
@@ -19,26 +28,32 @@ class BaseApi(object):
         return self
 
     def set_cookie(self, key, value):
+        self.cookies = self.cookies or {}
         self.cookies.update({key: value})
-        return self
-
-    def set_data(self, data):
-        self.data = data
         return self
 
     def set_json(self, json_data):
         self.json = json_data
         return self
 
-    def run(self, session=None):
+    def run(self, session = None):
         session = session or requests.sessions.Session()
         self.response = session.request(
             self.method,
             self.url,
             params=self.params,
-            cookies=self.cookies,
-            headers=self.headers,
             data=self.data,
+            headers=self.headers,
+            cookies=self.cookies,
+            files=self.files,
+            auth=self.auth,
+            timeout=self.timeout,
+            allow_redirects=self.allow_redirects,
+            proxies=self.proxies,
+            hooks=self.hooks,
+            stream=self.stream,
+            verify=self.verify,
+            cert=self.cert,
             json=self.json
         )
         return self
