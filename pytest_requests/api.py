6c740dd2490cccc9c5dc50f0181d51bd0c4e8638
debugtalk <mail@debugtalk.com>
2019-08-08 19:06:01 +0800

    feat: improve IDE grammar tips
    1, add ApiResponse
    2, make class instance variable private

--- pytest_requests/api.py
+++ pytest_requests/api.py
@@ -24,9 +24,6 @@ class BaseApi(object):
     cookies = None
     body = None
 
-    def __init__(self):
-        self.resp_obj = None
-
     def set_config(self, verify=None, auth=None, timeout=None, proxies=None,
             allow_redirects=True, hooks=None, stream=None, cert=None):
         """ set request configuration
@@ -42,7 +39,7 @@ class BaseApi(object):
             cert
 
         """
-        self._config = {
+        self.__config = {
             "verify": verify,
             "auth": auth,
             "timeout": timeout,
@@ -63,10 +60,12 @@ class BaseApi(object):
     def set_params(self, **params):
         """ update request params
         """
-        if not hasattr(self, "_params"):
-            self._params = copy.deepcopy(self.__class__.params or {})
+        try:
+            self.__params.update(params)
+        except AttributeError:
+            self.__params = copy.deepcopy(self.__class__.params or {})
+            self.__params.update(params)
 
-        self._params.update(params)
         return self
 
     def set_header(self, key, value):
@@ -78,10 +77,12 @@ class BaseApi(object):
     def set_headers(self, **headers):
         """ update request headers
         """
-        if not hasattr(self, "_headers"):
-            self._headers = copy.deepcopy(self.__class__.headers or {})
+        try:
+            self.__headers.update(headers)
+        except AttributeError:
+            self.__headers = copy.deepcopy(self.__class__.headers or {})
+            self.__headers.update(headers)
 
-        self._headers.update(headers)
         return self
 
     def set_cookie(self, key, value):
@@ -93,64 +94,92 @@ class BaseApi(object):
     def set_cookies(self, **kwargs):
         """ update request cookies
         """
-        if not hasattr(self, "_cookies"):
-            self._cookies = copy.deepcopy(self.__class__.cookies or {})
+        try:
+            self.__cookies.update(kwargs)
+        except AttributeError:
+            self.__cookies = copy.deepcopy(self.__class__.cookies or {})
+            self.__cookies.update(kwargs)
 
-        self._cookies.update(kwargs)
         return self
 
     def set_body(self, body):
         """ set request body
         """
-        self._body = body
+        self.__body = body
         return self
 
     def update_body(self, **kwargs):
         """
         """
-        self._body = parse_content(self.body, kwargs)
+        self.__body = parse_content(self.body, kwargs)
         return self
 
-    def run(self, session = None):
+    def __get_private_attribute(self, attr_name):
+        """ get private attribute value
 
+        Args:
+            attr_name (enum str): headers, params, cookies, body
+
+        """
+        try:
+            return getattr(self, "_BaseApi__{}".format(attr_name))
+        except AttributeError:
+            return getattr(self, attr_name)
+
+    def run(self, session=None):
+        """ make HTTP request with session
+        """
         session = session or requests.sessions.Session()
-        self._headers = getattr(self, "_headers", None) or self.headers
+        self.__headers = self.__get_private_attribute("headers")
 
         kwargs = {
-            "params": getattr(self, "_params", None) or self.params,
-            "headers": self._headers,
-            "cookies": getattr(self, "_cookies", None) or self.cookies
+            "params": self.__get_private_attribute("params"),
+            "headers": self.__headers,
+            "cookies": self.__get_private_attribute("cookies")
         }
-        if hasattr(self, "_config"):
-            kwargs.update(self._config)
 
-        _body = self._body if hasattr(self, "_body") else self.body
-        if isinstance(_body, dict):
-            if self._headers and \
-                self._headers.get("content-type", "").startswith("application/x-www-form-urlencoded"):
-                kwargs["data"] = json.dumps(_body)
+        try:
+            kwargs.update(self.__config)
+        except AttributeError:
+            pass
+
+        try:
+            resp_body = self.__body
+        except AttributeError:
+            resp_body = self.body
+
+        if isinstance(resp_body, dict):
+            if self.__headers and \
+                self.__headers.get("content-type", "").startswith("application/x-www-form-urlencoded"):
+                kwargs["data"] = json.dumps(resp_body)
             else:
-                kwargs["json"] = _body
+                kwargs["json"] = resp_body
         else:
-            kwargs["data"] = _body
+            kwargs["data"] = resp_body
 
         _resp_obj = session.request(
             self.method,
             self.url,
             **kwargs
         )
-        self.resp_obj = ResponseObject(_resp_obj)
-        return self
+        resp_obj = ResponseObject(_resp_obj)
+        return ApiResponse(resp_obj)
+
+
+class ApiResponse(object):
+
+    def __init__(self, resp_obj):
+        self.__resp_obj = resp_obj
 
     def extract_header(self, field):
         """ extract response header field.
         """
-        return self.resp_obj.extract_header(field)
+        return self.__resp_obj.extract_header(field)
 
     def extract_body(self, field):
         """ extract response body field, field supports jmespath
         """
-        return self.resp_obj.extract_body(field)
+        return self.__resp_obj.extract_body(field)
 
     def extract_(self, field):
         """ extract response field
@@ -160,12 +189,12 @@ class BaseApi(object):
                 e.g. status_code, headers.server, body.cookies.freeform
 
         """
-        return self.resp_obj.extract(field)
+        return self.__resp_obj.extract(field)
 
     def get_response_object(self):
         """ get response object.
         """
-        return self.resp_obj
+        return self.__resp_obj
 
     def __assert_with_expected(self, actual_value, expected_value):
         assert actual_value == expected_value
@@ -182,7 +211,7 @@ class BaseApi(object):
 
         """
         return self.__assert_with_expected(
-            self.resp_obj.extract(field),
+            self.__resp_obj.extract(field),
             expected_value
         )
 
@@ -198,7 +227,7 @@ class BaseApi(object):
 
         """
         return self.__assert_with_expected(
-            self.resp_obj.extract_header(field),
+            self.__resp_obj.extract_header(field),
             expected_value
         )
 
@@ -211,6 +240,6 @@ class BaseApi(object):
 
         """
         return self.__assert_with_expected(
-            self.resp_obj.extract_body(field),
+            self.__resp_obj.extract_body(field),
             expected_value
         )
