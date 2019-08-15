2f7ca63142e21b4ce9854ba5b80051d5da0ea252
debugtalk <mail@debugtalk.com>
2019-08-15 21:16:29 +0800

    feat: add validator greater_than(gt) and less_than(lt)

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,3 +1,4 @@
+import pytest
 from pytest_requests import validators
 
 
@@ -7,3 +8,19 @@ def test_validator_equals():
 
     func = validators.eq("abc")
     assert func("abc ".strip())
+
+
+def test_validator_greater_than():
+    func = validators.gt(400)
+    assert func(401)
+
+    with pytest.raises(AssertionError):
+        assert func(400)
+
+
+def test_validator_less_than():
+    func = validators.lt(400)
+    assert func(301)
+
+    with pytest.raises(AssertionError):
+        assert func(400)
